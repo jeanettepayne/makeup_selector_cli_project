5b8d27901e14b16f6ce5b1c0b0af614b971bb32e
Jeanette Payne <jeanetterpayne@gmail.com>
2020-09-08 23:52:27 +0000

    added class folders in correct location

---
+++ makeup_selector/lib/makeup_selector/cli.rb
@@ -0,0 +1,7 @@
+class CLI 
+  
+def initialize
+  puts "Welcome to the Makeup Selector App!"
+end
+  
+end
---
+++ makeup_selector/lib/makeup_selector/scraper.rb
@@ -0,0 +1,10 @@
+require 'httparty'
+response = HTTParty.get('http://makeup-api.herokuapp.com/api/v1/products.json')
+
+
+
+class Scraper
+  
+  
+  
+end
