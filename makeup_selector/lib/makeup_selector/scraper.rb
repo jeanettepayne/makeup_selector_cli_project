76b7e7104aa7f8e639bf58c560ffa0eae2a8e20d
Jeanette Payne <jeanetterpayne@gmail.com>
2020-09-09 23:48:31 +0000

    added ability to parse information from api for blush method to get data on blush products

--- makeup_selector/lib/makeup_selector/scraper.rb
+++ makeup_selector/lib/makeup_selector/scraper.rb
@@ -1,18 +1,28 @@
 require 'rubygems'
 require 'httparty'
+require 'pry'
 response = HTTParty.get('http://makeup-api.herokuapp.com/api/v1/products.json')
 
 
 class Scraper
   # include HTTParty
   
-response = HTTParty.get('http://makeup-api.herokuapp.com/api/v1/products.json')
+# response = HTTParty.get('http://makeup-api.herokuapp.com/api/v1/products.json')
 
-def blush
-  url = 'http://makeup-api.herokuapp.com/api/v1/products.json?type=blush'
+ def blush
+  url = 'http://makeup-api.herokuapp.com/api/v1/products.json?product_type=blush'
   response = HTTParty.get(url)
-  puts response
-end
+  # data = response.parsed_response
+    response.each do |product|
+      puts product["brand"]
+      puts product["name"]
+      puts product["price"]
+      puts product["rating"]
+      puts product["description"]
+    end
+  # puts response
+  # binding.pry
+ end
 
   
 end
