71eb93894157a695d7a79ef157ef2e3314cbbff1
Jeanette Payne <jeanetterpayne@gmail.com>
2020-09-09 01:56:49 +0000

    re-required httparty bc it's being a pain and I'm trying to figure out how to fix it

--- makeup_selector/lib/selector_environment.rb
+++ makeup_selector/lib/selector_environment.rb
@@ -1,5 +1,7 @@
 require_relative './makeup_selector/version'
 require_relative './makeup_selector/cli'
+require_relative './makeup_selector/scraper'
+require_relative './makeup_selector/makeup'
 
 module MakeupSelector
   class Error < StandardError; end
