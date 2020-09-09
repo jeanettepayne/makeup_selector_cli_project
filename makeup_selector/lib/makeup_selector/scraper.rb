1fcca4ad39db5206b4d490229576534ca157c684
Jeanette Payne <jeanetterpayne@gmail.com>
2020-09-09 01:12:29 +0000

    edits to scraper for httparty functionality

--- makeup_selector/lib/makeup_selector/scraper.rb
+++ makeup_selector/lib/makeup_selector/scraper.rb
@@ -1,10 +1,12 @@
+require 'rubygems'
 require 'httparty'
 response = HTTParty.get('http://makeup-api.herokuapp.com/api/v1/products.json')
 
 
-
 class Scraper
+  # include HTTParty
   
-  
+# response = HTTParty.get('http://makeup-api.herokuapp.com/api/v1/products.json')
+ 
   
 end
