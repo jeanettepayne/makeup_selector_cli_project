2c0b7e94efc66b06bf8a0b79756af323ac138d2d
Jeanette Payne <jeanetterpayne@gmail.com>
2020-09-09 01:37:02 +0000

    added product_list method

--- makeup_selector/lib/makeup_selector/cli.rb
+++ makeup_selector/lib/makeup_selector/cli.rb
@@ -2,6 +2,20 @@ class MakeupSelector::CLI
   
 def initialize
   puts "Welcome to the Makeup Selector App!"
+  self.product_list
+end
+
+def product_list
+  puts "Please enter a number to select a product type:
+  1. Blush
+  2. Bronzer
+  3. Eyebrow
+  4. Eyeliner
+  5. Eyeshadow
+  6. Foundation
+  7. Lip Liner 
+  8. Lipstick
+  9. Mascara"
 end
   
 end
