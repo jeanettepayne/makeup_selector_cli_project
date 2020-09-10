8ee5d3a8ea5b0beeb334011d5a8a0cd09a8cce6a
Jeanette Payne <jeanetterpayne@gmail.com>
2020-09-10 01:10:32 +0000

    un-hardcoded product list

--- makeup_selector/lib/makeup_selector/cli.rb
+++ makeup_selector/lib/makeup_selector/cli.rb
@@ -1,4 +1,4 @@
-class MakeupSelector::CLI 
+class CLI 
   
 def initialize
   puts "Welcome to the Makeup Selector App!
@@ -7,22 +7,13 @@ def initialize
 end
 
 def product_list
-  product_list = [blush, bronzer, eyebrow, eyeliner, eyeshadow, foundation, lip liner, lipstick, mascara]
+  product_array = ["Blush", "Bronzer", "Eyebrow", "Eyeliner", "Eyeshadow", "Foundation", "Lip Liner", "Lipstick", "Mascara"]
   
   puts "Please enter a number to select a product type:"
   
-  product_list.each.with_index(1) do |product, index|
+  product_array.each.with_index(1) do |product, index|
     puts "#{index}. #{product}"
   end
-  # 1. Blush
-  # 2. Bronzer
-  # 3. Eyebrow
-  # 4. Eyeliner
-  # 5. Eyeshadow
-  # 6. Foundation
-  # 7. Lip Liner 
-  # 8. Lipstick
-  # 9. Mascara
 end
 
 def get_user_product
@@ -37,3 +28,5 @@ end
 # end
   
 end
+
+CLI.new
