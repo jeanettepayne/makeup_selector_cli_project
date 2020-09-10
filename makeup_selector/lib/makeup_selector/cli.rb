8f12688a56edb6c15f6c45894a4686a13f9e86ee
Jeanette Payne <jeanetterpayne@gmail.com>
2020-09-10 00:46:48 +0000

    added get_user_product method to get user product choice

--- makeup_selector/lib/makeup_selector/cli.rb
+++ makeup_selector/lib/makeup_selector/cli.rb
@@ -6,25 +6,28 @@ def initialize
   self.product_list
 end
 
-# def product_list
-#   puts "Please enter a number to select a product type:
-#   1. Blush
-#   2. Bronzer
-#   3. Eyebrow
-#   4. Eyeliner
-#   5. Eyeshadow
-#   6. Foundation
-#   7. Lip Liner 
-#   8. Lipstick
-#   9. Mascara"
-# end
-
-def get_products
-  @products = MakeupSelector::Scraper.products
-end
-
 def product_list
-  puts "Please enter a number to select a product type:"
+  puts "Please enter a number to select a product type:
+  1. Blush
+  2. Bronzer
+  3. Eyebrow
+  4. Eyeliner
+  5. Eyeshadow
+  6. Foundation
+  7. Lip Liner 
+  8. Lipstick
+  9. Mascara"
 end
+
+def get_user_product
+  chosen_product = gets.strip.to_i 
+  if chosen_product == 1 
+    puts MakeupSelector::Scraper.blush 
+  end
+end
+
+# def get_products
+#   @products = MakeupSelector::Scraper.products
+# end
   
 end
