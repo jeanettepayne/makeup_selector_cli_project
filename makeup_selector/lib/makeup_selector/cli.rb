8bb74df375bed16fd7ff9cd04777d6897abab0e3
Jeanette Payne <jeanetterpayne@gmail.com>
2020-09-10 02:00:03 +0000

    editing methods to utilize makeup class and pass things around better

--- makeup_selector/lib/makeup_selector/cli.rb
+++ makeup_selector/lib/makeup_selector/cli.rb
@@ -1,4 +1,5 @@
 require_relative './scraper'
+require_relative './makeup'
 
 class CLI 
   
@@ -22,8 +23,26 @@ end
 def get_user_product
   chosen_product = gets.strip.to_i 
   if chosen_product == 1 
-    Scraper.new.blush 
+    Makeup.blush_category_info
+    # ADD OTHER OPTIONS HERE
   end
+end
+
+def self.search_choices
+  search_options = ["Brand", "Price", "Rating"]
+  puts "Enter a number to select your search parameters:"
+   search_options.each.with_index(1) do |option, index|
+    puts "#{index}. #{option}"
+  end
+    
+end
+
+def get_user_search_choice
+  # search_options = ["Brand", "Price", "Rating"]
+  # search_options.each.with_index(1) do |option, index|
+  #   puts "#{index}. #{option}"
+  # end
+  search_choice = gets.strip.to_i 
 end
 
 # def get_products
--- makeup_selector/lib/makeup_selector/makeup.rb
+++ makeup_selector/lib/makeup_selector/makeup.rb
@@ -1,7 +1,13 @@
+require_relative './cli'
+
 class Makeup
   attr_accessor :brand, :name, :price, :rating, :description
   
-  
+  def self.blush_category_info
+    # puts "Enter a number to select your search parameters:"
+    CLI.search_choices
+
+  end
   
   
 end
