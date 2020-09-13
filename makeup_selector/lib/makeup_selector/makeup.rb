3cc13e5191a7906e48aabcd032451f33d8cc5c18
Jeanette Payne <jeanetterpayne@gmail.com>
2020-09-13 17:13:21 +0000

    edits to initialize method and all class constant

--- makeup_selector/lib/makeup_selector/makeup.rb
+++ makeup_selector/lib/makeup_selector/makeup.rb
@@ -1,14 +1,34 @@
 require_relative './cli'
+require_relative './scraper'
+require 'pry'
 
 class Makeup
-  attr_accessor :brand, :name, :price, :rating, :description
+  attr_accessor :brand, :name, :price, :rating, :description, :product_category
+  
+  @@all = []
+  
+  def initialize(product_category, brand, name, price, rating, description)
+    @product_category = product_category
+    @brand = brand
+    @name = name
+    @price = price 
+    @rating = rating 
+    @description = description
+    @@all << self
+  end
   
   def self.blush_category_info
     # puts "Enter a number to select your search parameters:"
-    CLI.search_choices
-    CLI.get_user_search_choice
-
+    # CLI.search_choices
+    # CLI.get_user_search_choice
+   
   end
+  
+ def self.all
+   @@all
+ end
   
   
 end
+
+# Makeup.all
