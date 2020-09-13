49d73d22123018bac74f8f4178352c3a2b59cdf9
Jeanette Payne <jeanetterpayne@gmail.com>
2020-09-13 01:23:13 +0000

    added to brand_search method

--- makeup_selector/lib/makeup_selector/brand.rb
+++ makeup_selector/lib/makeup_selector/brand.rb
@@ -1,13 +1,27 @@
 class Brand
-  
+  attr_accessor :brand, :name, :price, :rating, :description
 
 def self.brand_search
   puts "Please type the name of the brand you'd like to search for (all lowercase letters):"
   brand_input = gets.strip
   url = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=#{brand_input}"
   response = HTTParty.get(url)
+  response.collect do |product|
+      {
+      :brand => product["brand"],
+      :name => product["name"],
+      :price => product["price"],
+      :rating => product["rating"],
+      :description => product["description"],
+      }
+    end 
+ puts "#{@name}"
 end
 
-  
+# def product_info
+#   puts "#{@name}
+#   #{@price}
+#   #{@rating}"
+# end
   
 end
