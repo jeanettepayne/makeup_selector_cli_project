3a2776957ef621d746aff3f31a4fa91725619ae1
Jeanette Payne <jeanetterpayne@gmail.com>
2020-09-13 01:11:02 +0000

    added brand_search method, built to take in user input and interpolate the entered brand into the url to pull info from the API

--- makeup_selector/lib/makeup_selector/brand.rb
+++ makeup_selector/lib/makeup_selector/brand.rb
@@ -2,7 +2,10 @@ class Brand
   
 
 def self.brand_search
-  puts "test output"
+  puts "Please type the name of the brand you'd like to search for (all lowercase letters):"
+  brand_input = gets.strip
+  url = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=#{brand_input}"
+  response = HTTParty.get(url)
 end
 
   
