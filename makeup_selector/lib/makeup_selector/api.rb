770d440da72721568f473d0747ab3d46d5fa39ee
Jeanette Payne <jeanetterpayne@gmail.com>
2020-09-14 23:13:37 +0000

    method name change to products, deleted old, unnecessary methods

--- makeup_selector/lib/makeup_selector/api.rb
+++ makeup_selector/lib/makeup_selector/api.rb
@@ -7,7 +7,7 @@ require 'pry'
 Module MakeupSelector
  class API
 
-def self.product_scrape
+def self.products
   response = HTTParty.get('http://makeup-api.herokuapp.com/api/v1/products.json')
   response.each do |product|
     product_category = product["product_type"]
@@ -17,125 +17,10 @@ def self.product_scrape
     rating = product["rating"]
     description = product["description"]
     
-    nm = Makeup.new(product_category, brand, name, price, rating, description)
+    nm = MakeupSelector::Makeup.new(product_category, brand, name, price, rating, description)
   # binding.pry
   end
 end
-
-
-
-
-# def blush
-#   url = 'http://makeup-api.herokuapp.com/api/v1/products.json?product_type=blush'
-#   response = HTTParty.get(url)
-#     response.collect do |product|
-#       {
-#       :brand => product["brand"],
-#       :name => product["name"],
-#       :price => product["price"],
-#       :rating => product["rating"],
-#       :description => product["description"],
-#       }
-#       # binding.pry
-#     end
-# end
-
-# def bronzer
-#   url = 'http://makeup-api.herokuapp.com/api/v1/products.json?product_type=bronzer'
-#   response = HTTParty.get(url)
-#     response.each do |product|
-#       product["brand"]
-#       product["name"]
-#       product["price"]
-#       product["rating"]
-#       product["description"]
-#       # binding.pry
-#     end
-# end
-
-# def eyebrow
-#   url = 'http://makeup-api.herokuapp.com/api/v1/products.json?product_type=eyebrow'
-#   response = HTTParty.get(url)
-#     response.each do |product|
-#       product["brand"]
-#       product["name"]
-#       product["price"]
-#       product["rating"]
-#       product["description"]
-#     end
-# end
- 
-# def eyeliner
-#   url = 'http://makeup-api.herokuapp.com/api/v1/products.json?product_type=eyeliner'
-#   response = HTTParty.get(url)
-#     response.each do |product|
-#       product["brand"]
-#       product["name"]
-#       product["price"]
-#       product["rating"]
-#       product["description"]
-#     end
-# end
- 
-# def eyeshadow
-#   url = 'http://makeup-api.herokuapp.com/api/v1/products.json?product_type=eyeshadow'
-#   response = HTTParty.get(url)
-#     response.each do |product|
-#       product["brand"]
-#       product["name"]
-#       product["price"]
-#       product["rating"]
-#       product["description"]
-#     end
-# end
- 
-# def foundation
-#   url = 'http://makeup-api.herokuapp.com/api/v1/products.json?product_type=foundation'
-#   response = HTTParty.get(url)
-#     response.each do |product|
-#       product["brand"]
-#       product["name"]
-#       product["price"]
-#       product["rating"]
-#       product["description"]
-#     end
-# end
- 
-# def lip_liner
-#   url = 'http://makeup-api.herokuapp.com/api/v1/products.json?product_type=lip_liner'
-#   response = HTTParty.get(url)
-#     response.each do |product|
-#       product["brand"]
-#       product["name"]
-#       product["price"]
-#       product["rating"]
-#       product["description"]
-#     end
-# end
- 
-# def lipstick
-#   url = 'http://makeup-api.herokuapp.com/api/v1/products.json?product_type=lipstick'
-#   response = HTTParty.get(url)
-#     response.each do |product|
-#       product["brand"]
-#       product["name"]
-#       product["price"]
-#       product["rating"]
-#       product["description"]
-#     end
-# end
- 
-# def mascara
-#   url = 'http://makeup-api.herokuapp.com/api/v1/products.json?product_type=mascara'
-#   response = HTTParty.get(url)
-#     response.each do |product|
-#       product["brand"]
-#       product["name"]
-#       product["price"]
-#       product["rating"]
-#       product["description"]
-#     end
-# end
  
   
 end
