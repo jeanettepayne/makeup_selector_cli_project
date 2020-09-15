eff87723c0aa8ba5481039b3fa351412d157d448
Jeanette Payne <jeanetterpayne@gmail.com>
2020-09-15 01:13:16 +0000

    working on get_description method

--- makeup_selector/lib/makeup_selector/cli.rb
+++ makeup_selector/lib/makeup_selector/cli.rb
@@ -5,7 +5,7 @@ require 'pry'
 module MakeupSelector
  class CLI 
   attr_accessor :chosen_category
-  attr_reader :categories, :product_category, :brand, :name, :price, :rating, :description, :category_list
+  attr_reader :categories, :product_category, :brand, :name, :price, :rating, :description, :category_list, :product_list
   
   def start
     puts "Welcome to the Makeup Selector App!
@@ -14,7 +14,7 @@ module MakeupSelector
     self.get_categories
     self.category_list
     self.get_user_category
-    #self.get_user_description
+    self.get_description
   end
   
    def get_categories
@@ -38,6 +38,7 @@ module MakeupSelector
     product_type = @category_list[input-1]
     puts "Here is a list of #{product_type} products:"
     
+    @product_list = []
     counter = 1
     MakeupSelector::Makeup.all.each do |product, index|
       if product_type == product.product_category
@@ -45,13 +46,18 @@ module MakeupSelector
         Brand: #{product.brand}, 
         Price: $#{product.price}"
         counter += 1
+        @product_list.push
       end
      end
     end
     
-    def get_user_description
+    def get_description
       puts "To get the description for any product above, please enter its list number:"
       input = gets.strip.to_i
+      
+      product_chosen = @product_list[input-1]
+      binding.pry
+        puts "#{product.description}"
     end
 
   
